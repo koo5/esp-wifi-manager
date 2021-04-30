c966f6fc87103d2091f055f6283870fb9d899fb6
koo5 <kolman.jindrich@gmail.com>
2021-04-30 18:53:39 +0200

    breaking stuff

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,14 +1,15 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    public: {url: '/', static: true},
+    //src: {url: '/dist'},
   },
   plugins: [
      '@snowpack/plugin-svelte'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
     bundle: true,
